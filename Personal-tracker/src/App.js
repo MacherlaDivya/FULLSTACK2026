a9f9c853eb977e0f28a6d8d6c50a3632012d9a31
MacherlaDivya <2303a51461@sru.edu.in>
2026-02-09 10:36:05 +0530

    Add files via upload

---
+++ Personal-tracker/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Dashboard from './dashboard'; // Import the Dashboard component
+
+const App = () => {
+  return ( 
+    <div className="App">
+      <Dashboard />  {/* Use the Dashboard component here */}
+    </div>
+  );
+};
+
+export default App;
