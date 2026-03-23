84e32dab83f4ea4551d54a73cb5e0571ff699335
MacherlaDivya <2303a51461@sru.edu.in>
2026-03-23 10:07:49 +0530

    Delete DevOps_2026-main/Project directory

--- DevOps_2026-main/Project/smart-city-energy-dashboard/frontend/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-const Loader = ({ label = 'Loading smart city insights...' }) => (
-  <div className="loader-shell">
-    <div className="loader-ring" />
-    <p>{label}</p>
-  </div>
-);
-
-export default Loader;
